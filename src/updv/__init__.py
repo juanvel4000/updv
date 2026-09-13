46b7b564710bd570eb8713cf9b66351da8b42279
juanvel400 <me@juanvel400.com>
2026-09-13 15:47:05 -0500

    bump version values to 0.2.0

--- src/updv/__init__.py
+++ src/updv/__init__.py
@@ -2,7 +2,7 @@ from .cli import main
 from .engine import ProcessResult, process_config, process_file
 from .parser import Configuration, FileDescriptor
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "Configuration",
     "FileDescriptor",
