1a99caf0f2f8bf4d87ceefc283b670c56dce77f0
juanvel400 <me@juanvel400.com>
2026-09-14 18:47:18 -0500

    update version values to 0.4.0

--- src/updv/__init__.py
+++ src/updv/__init__.py
@@ -2,7 +2,7 @@ from .cli import main
 from .engine import ProcessResult, process_config, process_file
 from .parser import Configuration, FileDescriptor
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __all__ = [
     "Configuration",
     "FileDescriptor",
